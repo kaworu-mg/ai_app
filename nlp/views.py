3b7879f8a1ee9a9efd1afb5e7942eb183fc914b5
kaworu-mg <70263755+kaworu-mg@users.noreply.github.com>
2021-02-10 16:09:04 +0900

    Update views.py

--- nlp/views.py
+++ nlp/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import joblib
 import pandas as pd
 import pickle
 # Create your views here.
 category_data = pd.read_csv("idx2category.csv")
 idx2category = {row.k: row.v for idx, row in category_data.iterrows()}
 
-with open("rdmf.pickle", mode="rb") as f:
-  model = pickle.load(f)
+model = joblib.load("rdmf.pickle")
 
 def index(request):
   if request.method == "GET":
